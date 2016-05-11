4219a44a220f59465b9ddc9cfe914cbf9e5f5cba
tazzon <tazzon@free.fr>
2016-05-11 17:08:45 +0200

    modification du texte d'aide pour la page nouvelle série

--- js/global.js
+++ js/global.js
@@ -34,7 +34,7 @@ var isave = {
 };
 var infoapp= {
   version : '0.3.1',
-  datecode : 20160509.02,
+  datecode : 20160511.01,
   name : "Sago",
   mail : "tazzon@free.fr",
   git : 'https://github.com/tazzon/sago',
@@ -81,7 +81,7 @@ var zoomH;
 var zoomW;
 
 // les textes d'aide en fonction de l'onglet ou l'on se trouve
-var help_session='<p>Choisissez les paramètres de votre session (nombre de volées, flèches par volée, distance, diamètre du blason).</p><p>Le diamètre du tube est respecté à l’affichage lors de la saisie des flèches, soyez le plus près possible de la réalité.</p><p>Si vous cochez la case « Spot », votre blason complet sera limité au 6 ou au 5 suivant votre choix.</p><p>Vous pouvez enfin choisir d’utiliser un petit 10 en cochant la case « Petit 10 ».</p>';
+var help_session='<p>Choisissez les paramètres de votre session (nombre de volées, flèches par volée, distance, diamètre du blason).</p><p>Le diamètre du tube est respecté à l’affichage lors de la saisie des flèches, soyez le plus près possible de la réalité.</p><p>Si vous avez un objectif, vous pouvez cocher la case « Objectif de points » et indiquer la valeur en face.</p><p>Si vous cochez la case « Spot », votre blason complet sera limité au 6 ou au 5 suivant votre choix.</p><p>Vous pouvez enfin choisir d’utiliser un petit 10 en cochant la case « Petit 10 ».</p>';
 var help_tab_score='<p>La feuille de marque permet de voir les volées qui ont été faite et validées dans l’onglet de saisie.</p><p>Vous pouvez ajouter un commentaire (icône <span class="icon icon-comment"></span>) par volée (changement de réglage, sensation, etc).</p><p>Vous pouvez sauvegarder à tout moment la volée en cours grâce au bouton <span class="icon icon-floppy"></span>.Le nom d’une série non enregistrée est suivie d’un astérisque (*).</p><p>Le bouton <span class="icon icon-bullseye"></span> permet un accès direct à la saisie de la volée en cours.</p>';
 var help_saisie='<p>Les flèches peuvent être saisies dans n’importe quel ordre (numéro de flèche par exemple), elles seront triées automatiquement dans la feuille de marque lors de la validation de la volée par le bouton <span class="icon icon-ok"></span>.</p><p>Lors de la saisie d’une flèche, si celle-ci se trouve hors de la cible visible, déplacez votre doigt hors de la cible pour ajouter des zones. Un passage rapide dans le jaune supprimera une à une les zones ajoutées. À la fin de chaque flèche, le blason reprendra l’aspect choisi dans les options.</p><p>Vous pouvez recommencer la dernière flèche en appuyant sur le bouton <span class="icon icon-reply"></span> ou toute la volée grace au bouton<span class="icon icon-reply-all"></span>.</p><p>Vous pouvez ajouter un commentaire à la volée en cours (avant de la valider) avec le bouton <span class="icon icon-comment"></span>, il sera consultable ou modifiable ultérieurement dans la feuille de marque.</p><p>Le bouton <span class="icon icon-table"></span> permet un accès direct à la feuille de marque de la volée en cours.</p>';
 var help_options='<p>Augmenter au diminuez la taille globale de l’affichage pour l’ensemble de l’application avec les boutons <span class="icon icon-zoom-out"></span> ou <span class="icon icon-zoom-in"></span>.</p><p>Si vous maintenez cochée la case « sauvegarde automatique », à chaque validation d’une volée, la série sera sauvegardée.<p><p>Dans le cas d’une utilisation en extérieur (ou suivant vos goûts), il peut être utile de passer l’interface en contraste élevé.</p><p>Vous pouvez choisir le nombres de zones visible du blason lors de la saisie des flèches.</p>';
